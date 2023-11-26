21ed170f76422eee0bf14fcb23d3fec8bec456e8
akrzelj-ossunist <krzelj99antonio@gmail.com>
2023-11-26 02:21:07 +0100

    login and register in work

--- SmoothSailing/src/main/java/com/SmoothSailing/controllers/UserController.java
+++ SmoothSailing/src/main/java/com/SmoothSailing/controllers/UserController.java
@@ -1,15 +1,58 @@
 package com.SmoothSailing.controllers;
 
+
+import com.SmoothSailing.models.UserModel;
 import com.SmoothSailing.repositories.UserRepo;
+import com.SmoothSailing.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping(path="/user")
+@CrossOrigin
 public class UserController {
+
+    private final UserService userService;
     private final UserRepo userRepo;
 
     @Autowired
-    public UserController(UserRepo userRepo){
+    public UserController(UserService userService, UserRepo userRepo){
+        this.userService = userService;
         this.userRepo=userRepo;
     }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("registerRequest", new UserModel());
+        return "register_page";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model){
+        model.addAttribute("loginRequest", new UserModel());
+        return "login_page";
+    }
+
+    @PostMapping(path="/register")
+    public String register(@RequestBody UserModel userModel){
+        System.out.println("register request: " + userModel);
+        UserModel registeredUser = userService.registerUser(userModel.getName(), userModel.getSurname(), userModel.getPassword(), userModel.getEmail(),userModel.getBirthday(),userModel.getGender(),userModel.getLicense());
+        return "req 200";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute UserModel userModel, Model model){
+        System.out.println("login request: " + userModel);
+        UserModel authenticated = userService.authenticate(userModel.getEmail(), userModel.getPassword());
+        if(authenticated!=null){
+            model.addAttribute("userEmail", authenticated.getEmail());
+            return "personal_page";
+        }
+        else{
+            return "error_page";
+        }
+    }
+
 }
--- SmoothSailing/src/main/java/com/SmoothSailing/services/UserService.java
+++ SmoothSailing/src/main/java/com/SmoothSailing/services/UserService.java
@@ -1,8 +1,13 @@
 package com.SmoothSailing.services;
 
+import com.SmoothSailing.models.UserModel;
 import com.SmoothSailing.repositories.UserRepo;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,4 +17,42 @@ public class UserService {
     public UserService(UserRepo userRepo){
         this.userRepo=userRepo;
     }
+
+    public UserModel registerUser(String name, String surname, String password, String email, Date birthday, String gender, String license){
+        if(userRepo.findByEmail(email).isPresent()){
+            System.out.println("Email vec postoji u bazi!");
+            return null;
+        }
+        if (name !=null && surname!=null && password!=null && email!=null){
+            UserModel userModel = new UserModel();
+            userModel.setName(name);
+            userModel.setSurname(surname);
+            userModel.setPassword(BCrypt.hashpw(password, BCrypt.gensalt(10)));
+            userModel.setEmail(email);
+            userModel.setBirthday(birthday);
+            userModel.setGender(gender);
+            userModel.setLicense(license);
+            return userRepo.save(userModel);
+        }
+        else{
+            return null;
+        }
+    }
+
+
+    public UserModel authenticate(String email, String password) {
+        System.out.println("Authenticating user with email: " + email);
+        System.out.println("Authenticating user with password: " + password);
+
+        Optional<UserModel> userOptional = userRepo.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            UserModel user = userOptional.get();
+            if (BCrypt.checkpw(password, user.getPassword())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
 }
